c9f7de300b05f6e37caad2d36bd5018926133d35
Sabuhi0 <sabuhiq0@gmail.com>
2022-10-23 01:24:31 +0400

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar';
+import Challenges from './views/Challenges';
+import Results from './views/Results';
+import SpeedCheck from './views/SpeedCheck';
 
 const App = () => {
   return (
     <div className="mx-auto">
       <BrowserRouter>
         <Navbar />
+        <Routes>
+          <Route path='/results' element={<Results />} />
+          <Route path='/challenges' element={<Challenges />} />
+          <Route path='/' element={<SpeedCheck />} />
+        </Routes>
       </BrowserRouter>
       <main></main>
     </div>
