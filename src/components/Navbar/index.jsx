e659da1bf0e14265e89d82a6f76b3f7e0d01f538
Sabuhi0 <sabuhiq0@gmail.com>
2022-10-27 15:15:31 +0400

    Added NavLink

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,8 +1,16 @@
 import React, { useState } from "react";
 import { Transition } from "@headlessui/react";
+import { NavLink, useLocation } from "react-router-dom";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  
+  const location = useLocation();
+
+  const { pathname } = location
+
+  const splitLocation = pathname.split("/");
+
   return (
     <nav className="bg-gray-800">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -17,26 +25,26 @@ const Navbar = () => {
             </div>
             <div className="hidden md:block">
               <div className="ml-10 flex items-baseline space-x-4">
-                <a  
-                  href="#"
-                  className=" hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
+                <NavLink  
+                  to="/"
+                  className={`hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium ${splitLocation[1] === "" ? "bg-gray-700" : ""}`}
                 >
                   Home
-                </a>
+                </NavLink>
 
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                <NavLink
+                  to="/results"
+                  className={`hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium ${splitLocation[1] === "results" ? "bg-gray-700" : ""}`}
                 >
                   Results
-                </a>
+                </NavLink>
 
-                <a 
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                <NavLink
+                  to="/challenges"
+                  className={`hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium ${splitLocation[1] === "challenges" ? "bg-gray-700" : ""}`}
                 >
                   Texts
-                </a>
+                </NavLink>
               </div>
             </div>
           </div>
